dc7f6ea10f08bd3583b12210166b2d3ddf888e8a
Nikhilltiwari <nikhilt839@gmail.com>
2024-07-19 01:06:34 +0530

    suppress tensorflow info

--- run.py
+++ run.py
@@ -1,3 +1,7 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'  # Suppress TensorFlow INFO and WARNING messages
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'  # Disable oneDNN custom operations
+
 from app import create_app
 
 app = create_app()
